5be02f15bc3323837e694d01fce596cc2da48de3
sobhan <sobhan.n@gmail.com>
2025-11-16 08:12:46 -0500

    Update json for postman

--- webhook-event-publisher-api/api/src/main/java/com/java/barclaycardus/webhooksvcs/pubsrc/validation/SchemaValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.java.barclaycardus.webhooksvcs.pubsrc.validation;
-
-public class SchemaValidationException extends RuntimeException {
-    public SchemaValidationException(String message) {
-        super(message);
-    }
-}
